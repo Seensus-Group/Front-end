c3b3cf3a7a00f6c1d7c7a8866335adbf21e638e9
Isack Santos <101467942+Isack2022@users.noreply.github.com>
2023-07-29 16:04:12 -0300

    Feature: Criando a parte de login

---
+++ js/index.js
@@ -0,0 +1,49 @@
+const btnLogin = document.getElementById("btnLogin");
+const txtuser = document.getElementById("txtemail");
+const senha = document.getElementById("txtsenha");
+let autenticado;
+let token;
+let key;
+
+btnLogin.onclick = () => {
+    if (txtuser.value.trim() == "" || senha.value.trim() == "") {
+        return alert(`Todos os campos devem ser preenchidos`)
+    }
+    else {
+        fetch("http://127.0.0.1:5062/users/login", {
+            method: "POST",
+            headers: {
+                "accept": "application/json",
+                "content-type": "application/json"
+            },
+            body: JSON.stringify({
+                email: txtuser.value,
+                senha: senha.value
+            })
+        }).then((response) => response.json()).then((dado) => {
+            if (dado.output == "Authenticated") {
+                autenticado = true;
+                token = dado.token;
+                txtuser.value = "";
+                senha.value = "";
+                window.location.replace(`index.html?key+${token}`);
+            }
+            else {
+                txtuser.value = "";
+                senha.value = "";
+                return alert(`Usuário ou seenha incorretos`);
+            }
+        }).catch((error) => {
+            console.error(`Não foi possível requisitar a API -> ${error}`);
+        });
+    };
+};
+
+document.getElementById("login").onclick = () => {
+    document.getElementById("shadow").style.zIndex = "200";
+    document.getElementById("shadow").style.opacity = "1";
+}
+document.getElementById("fechar").onclick = () => {
+    document.getElementById("shadow").style.zIndex = "-100";
+    document.getElementById("shadow").style.opacity = "0";
+}
