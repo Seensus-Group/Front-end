22184face50781e30162bf3b5c041a4a0c6b9852
ISACK CARDOSO DOS SANTOS <isack.csantos@senacsp.edu.br>
2023-08-07 11:51:56 -0300

    Feature: Criando área exclusiva

--- js/index.js
+++ js/index.js
@@ -3,8 +3,6 @@ const txtuser = document.getElementById("txtemail");
 const senha = document.getElementById("txtsenha");
 const txtbuscar = document.getElementById("txtBuscar");
 const btnbuscar = document.getElementById("btnBuscar");
-const btnEditar = document.getElementById("editar");
-const btnDados = document.getElementById("btnDados");
 let autenticado;
 let token;
 let key = "";
@@ -47,6 +45,7 @@ document.getElementById("login").onclick = () => {
     document.getElementById("shadow").style.zIndex = "200";
     document.getElementById("shadow").style.opacity = "1";
 }
+
 document.getElementById("fechar").onclick = () => {
     document.getElementById("shadow").style.zIndex = "-100";
     document.getElementById("shadow").style.opacity = "0";
@@ -57,53 +56,53 @@ document.getElementById("fechar").onclick = () => {
 function carregarDados() {
     key = window.location.search.substring(5, window.location.search.length);
     const estrutura = document.getElementById("estrutura");
-    
+
     let url1 = `http://127.0.0.1:5062/store/list`;
     let url2 = `http://127.0.0.1:5062/store/listbyname/${key}`;
-    let url="";
-    if(key=="" || key==null || !key){
+    let url = "";
+    if (key == "" || key == null || !key) {
         url = url1;
     }
-    else{
+    else {
         url = url2;
     }
-        
-        estrutura.innerHTML="";
 
-        fetch(url, {
-            method: "GET",
-            headers: {
-                "accept": "application/json",
-                "content-type": "application/json"
-            }
-        }).then((response) => response.json()).then((result) => {
-            result.data.map((item, index) => {
-                
-                let divList = document.createElement("div");
-                divList.style.height = "150px";
-                divList.setAttribute("class", "card m-3");
-                divList.innerHTML = `
+    estrutura.innerHTML = "";
+
+    fetch(url, {
+        method: "GET",
+        headers: {
+            "accept": "application/json",
+            "content-type": "application/json"
+        }
+    }).then((response) => response.json()).then((result) => {
+        result.data.map((item, index) => {
+
+            let divList = document.createElement("div");
+            divList.style.height = "150px";
+            divList.setAttribute("class", "card m-3");
+            divList.innerHTML = `
                 <a href="lojas.html?pag=${item.idloja}" class="nav-link">
-                    <div class="row">
-                        <div class="col-md-4">
-                            <img src="${item.foto}" class="img-fluid rounded" style="height: 150px;width:400px;object-fit:contain" alt="...">
-                        </div>
-                        <div class="col-md-8">
-                            <div class="card-body">
-                                <h5 class="card-title">${item.nome}</h5>
-                                <p class="card-text">${item.descricao}</p>
-                            </div>
+                <div class="row">
+                    <div class="col-md-4">
+                        <img src="${item.foto}" class="img-fluid rounded" style="height: 150px;width:400px;object-fit:contain" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body ">
+                            <h5 class="card-title">${item.nome}</h5>
+                            <p class="card-text">${item.descricao}</p>
                         </div>
                     </div>
-                </a>
+                </div>
+            </a>
                 `;
-                estrutura.appendChild(divList);
-            })
-        }).catch((error) => console.log(`Erro ao executar API -> ${error}`));
+            estrutura.appendChild(divList);
+        })
+    }).catch((error) => console.log(`Erro ao executar API -> ${error}`));
 
 };
 
-btnbuscar.onclick = () =>{
+btnbuscar.onclick = () => {
     window.location.replace(`listar.html?pag=${txtbuscar.value}`);
 }
 
@@ -113,7 +112,7 @@ function carregarCategoria() {
 
     const estrutura = document.getElementById("estrutura");
 
-    fetch("http://127.0.0.1:5062/store/listbycategory/"+key, {
+    fetch("http://127.0.0.1:5062/store/listbycategory/" + key, {
         method: "GET",
         headers: {
             "accept": "application/json",
@@ -127,11 +126,11 @@ function carregarCategoria() {
             divList.innerHTML = `
             <a href="lojas.html?pag=${item.idloja}" class="nav-link">
                 <div class="row">
-                    <div class="col-4">
+                    <div class="col-md-4">
                         <img src="${item.foto}" class="img-fluid rounded" style="height: 150px;width:400px;object-fit:contain" alt="...">
                     </div>
-                    <div class="col-8">
-                        <div class="card-body ">
+                    <div class="col-md-8">
+                        <div class="card-body">
                             <h5 class="card-title">${item.nome}</h5>
                             <p class="card-text">${item.descricao}</p>
                         </div>
@@ -146,10 +145,10 @@ function carregarCategoria() {
 
 
 function infoLojas() {
-    key = window.location.search.substring(5, window.location.search.length);   
+    key = window.location.search.substring(5, window.location.search.length);
     const estruturaLojas = document.getElementById("dadosLojas");
-    
-    fetch("http://127.0.0.1:5062/store/listbyid/"+key, {
+
+    fetch("http://127.0.0.1:5062/store/listbyid/" + key, {
         method: "GET",
         headers: {
             "accept": "application/json",
@@ -181,52 +180,6 @@ function infoLojas() {
     }).catch((error) => console.log(`Erro ao executar API -> ${error}`));
 }
 
-function editar(id, nome, email,descricao,cnpj,logradouro,cep,categoria){
-    
-    let divList = document.createElement("div");
-    divList.innerHTML = `
-    <div class="card m-3">
-        <div class="row p-2">
-            <div class="col">
-                <img src="${item.foto}" class="img-fluid rounded" style="height: 200px;width:400px;object-fit:contain" alt="...">
-            </div>
-            <div class="col-8">
-                <div class="card-body">
-                    <h5 class="card-title text-center">${item.nome}</h5>
-                    <p class="card-text"><strong>Sobre a loja: </strong>${item.descricao}</p>
-                    <p class="card-text"><strong>Telefone: </strong>${item.telefone} <strong>CNPJ da loja: </strong>${item.cnpj}</p>
-                    <p class="card-text"><strong>Logradouro: </strong>${item.logradouro} <strong>CEP: </strong>${item.cep}</p>
-                    <p class="card-text"><strong>Departamento: </strong>${item.categoria}</p>
-                </div>
-            </div>
-        </div>
-    </div>
-    `;
-    estruturaLojas.appendChild(divList);
-   
-    input_sub.onclick = ()=>{
-        if(input_cpass.value != input_pass.value){
-            input_cpass.value= "";
-            return alert("Senha de confirmaçãom incorreta");
-        }
-        else{
-            fetch(`http://127.0.0.1:30021/users/update/${id}`,{
-                method:"PUT",
-                headers:{
-                    "accept":"application/json",
-                    "content-type":"application/json",
-                    "token": key
-                },
-                body:JSON.stringify({
-                    email:input_email.value,
-                    senha:input_pass.value
-                })
-            }).then
-        }
-    }
-}
-
-btnEditar.onclick = () =>{
-    document.getElementById("shadow").style.zIndex = "200";
-    document.getElementById("shadow").style.opacity = "1";
+function editarLojas () {
+    key = window.location.search.substring(5, window.location.search.length);
 }
