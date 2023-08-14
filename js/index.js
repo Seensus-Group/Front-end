5d331c5a35860c1ca74df3ff4a3c8fb3c1bff8d1
ISACK CARDOSO DOS SANTOS <isack.csantos@senacsp.edu.br>
2023-08-14 11:53:45 -0300

    Feature: Termino da Área exlusiva e correção de alguns bugs

--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
+// Inicio dos dados login
 const btnLogin = document.getElementById("btnLogin");
 const txtuser = document.getElementById("txtemail");
 const senha = document.getElementById("txtsenha");
+// Fim dos dados Login
+
+// Inicio dos dados de editar
 const txtNome = document.getElementById("txtNome");
 const txtEmail = document.getElementById("txtEmail");
 const txtTelefone = document.getElementById("txtTelefone");
@@ -11,13 +15,22 @@ const txtComplemento = document.getElementById("txtComplemento");
 const txtPassword = document.getElementById("txtPasswd");
 const txtDescricao = document.getElementById("txtDescricao");
 const btnEditar = document.getElementById("btnEditar");
+// Fim dos dados de editar
+
+
+// Inicio dos dados da barra de pesquisa
 const txtbuscar = document.getElementById("txtBuscar");
 const btnbuscar = document.getElementById("btnBuscar");
+// Fim dados da barra de pesquisa
+
+// Inicio dos dados dos token
 let autenticado;
 let token;
 let key = "";
+// Fim dos dados dos token
 
-btnLogin.onclick = () => {
+// Inicio da função de login
+function login () {
   if (txtuser.value.trim() == "" || senha.value.trim() == "") {
     return alert(`Todos os campos devem ser preenchidos`);
   } else {
@@ -53,17 +66,23 @@ btnLogin.onclick = () => {
       });
   }
 };
+// Fim da função de login
 
-document.getElementById("login").onclick = () => {
+// Incio abrir Shadow
+function showShadow() {
   document.getElementById("shadow").style.zIndex = "200";
   document.getElementById("shadow").style.opacity = "1";
 };
+// fim do abrir Shadow
 
-document.getElementById("fechar").onclick = () => {
+// Inicio do fechar shadow
+function closedShadow() {
   document.getElementById("shadow").style.zIndex = "-100";
   document.getElementById("shadow").style.opacity = "0";
 };
+// Fechar o shadow
 
+// Função da barra de pesquisa
 function carregarDados() {
   key = window.location.search.substring(5, window.location.search.length);
   const estrutura = document.getElementById("estrutura");
@@ -116,7 +135,9 @@ function carregarDados() {
 btnbuscar.onclick = () => {
   window.location.replace(`listar.html?pag=${txtbuscar.value}`);
 };
+// Fim da função da barra de pesquisa
 
+// Inicio da função categoria
 function carregarCategoria() {
   key = window.location.search.substring(5, window.location.search.length);
 
@@ -155,7 +176,9 @@ function carregarCategoria() {
     })
     .catch((error) => console.log(`Erro ao executar API -> ${error}`));
 }
+// Fim da função categoria
 
+// Função carregar o id da loja
 function infoLojas() {
   key = window.location.search.substring(5, window.location.search.length);
   const estruturaLojas = document.getElementById("dadosLojas");
@@ -194,25 +217,70 @@ function infoLojas() {
     })
     .catch((error) => console.log(`Erro ao executar API -> ${error}`));
 }
+// Fim da função carregar o id da loja
 
-function infoLojaEditar() {
-  const url = window.location.search;
+
+// Execução para fazer update
+function editar() {
+  let url = window.location.search;
   let params = new URLSearchParams(url);
   let id = parseInt(params.get("pag"));
-  const estruturaLojas = document.getElementById("dadosLojas");
+  if (txtNome == "" || txtNome == "" || txtNome == null) {
+    alert("Erro ao atulizar os dados")
+  }
+  else{
+    alert("atualizou os dados");
+    window.location.reload(true);
+    fetch(`http://127.0.0.1:5062/store/update/${id}`, {
+      method: "PUT",
+      headers: {
+        accept: "application/json",
+        "content-type": "application/json",
+        token: key,
+      },
+      body: JSON.stringify({
+        nome: txtNome.value,
+        email: txtEmail.value,
+        telefone: txtTelefone.value,
+        cep: txtCep.value,
+        numero: txtNumero.value,
+        logradouro: txtLogradouro,
+        complemento: txtComplemento,
+        senha: txtPassword.value,
+        descricao: txtDescricao.value,
+      }),
+    }).then.catch((error) => console.log(`Erro ao executar API -> ${error}`));
+  }
+}
+// Fim do update
 
-  fetch("http://127.0.0.1:5062/store/listbyid/" + id, {
-    method: "GET",
-    headers: {
-      accept: "application/json",
-      "content-type": "application/json",
-    },
-  })
-    .then((response) => response.json())
-    .then((result) => {
-      result.data.map((item, index) => {
-        let divList = document.createElement("div");
-        divList.innerHTML = `
+// Função carregar o id da loja e passa os dados
+function infoLojaEditar() {
+    let url = window.location.search;
+    let params = new URLSearchParams(url);
+    let id = parseInt(params.get("pag"));
+    const estruturaLojas = document.getElementById("dadosLojas");
+
+    fetch("http://127.0.0.1:5062/store/listbyid/" + id, {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        "content-type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        result.data.map((item, index) => {
+          document.getElementById("txtNome").value = item.nome;
+          document.getElementById("txtTelefone").value = item.telefone;
+          document.getElementById("txtNumero").value = item.numero;
+          document.getElementById("txtComplemento").value = item.complemento;
+          document.getElementById("txtEmail").value = item.email;
+          document.getElementById("txtCep").value = item.cep;
+          document.getElementById("txtLogradouro").value = item.logradouro;
+          document.getElementById("txtDescricao").value = item.descricao;
+          let divList = document.createElement("div")
+          divList.innerHTML = `
         <div class="card m-3">
         <div class="row p-2">
             <div class="col-md-4">
@@ -222,8 +290,8 @@ function infoLojaEditar() {
                 <div class="card-body">
                     <div class="d-flex justify-content-around">
                         <h5 class="card-title"  >${item.nome}</h5>
-                        <a href="javascript:void" id="login">
-                            <i class="bi bi-pencil-square">Editar dados</i>
+                        <a href="#" id="editar" onclick="showShadow()">
+                            <i class="bi bi-pencil-square"></i>Editar dados
                         </a>
                     </div>
                     <p class="card-text"><strong>Sobre a loja: </strong>${item.descricao}</p>
@@ -235,50 +303,10 @@ function infoLojaEditar() {
             </div>
             </div>
             `;
-        estruturaLojas.appendChild(divList);
-      });
-    })
-    .catch((error) => console.log(`Erro ao executar API -> ${error}`));
+          estruturaLojas.appendChild(divList);
+        });
+      })
+      .catch((error) => console.log(`Erro ao executar API -> ${error}`));
 }
+// Fim da função carregar o id da loja e passa os dados
 
-// btnEditar.onclick = () => {
-//   key = window.location.search.substring(5, window.location.search.length);
-//   const estruturaEditar = document.getElementById("editarLojas");
-//   // Pegando o id da url
-//   // const url = window.location.search;
-//   // let params = new URLSearchParams(url);
-//   // let id = parseInt(params.get("id"));
-//   // alert(id);
-//   fetch(`http://127.0.0.1:5062/store/update/${id}`, {
-//     method: "PUT",
-//     headers: {
-//       accept: "application/json",
-//       "content-type": "application/json",
-//       token: key,
-//     },
-//     body: JSON.stringify({
-//       nome: txtNome.value,
-//       email: txtEmail.value,
-//       telefone: txtTelefone.value,
-//       cep: txtCep.value,
-//       numero: txtNumero.value,
-//       logradouro: txtLogradouro,
-//       complemento: txtComplemento,
-//       senha: txtPassword.value,
-//       descricao: txtDescricao.value,
-//     }),
-//   })
-//     .then((response) => response.json())
-//     .then((result) => {
-//       result.data.map(() => {
-//         if ((txtPassword = "")) {
-//           alert("Os dados da loja foram atualizados");
-//         } else {
-//           txtuser.value = "";
-//           senha.value = "";
-//           return alert(`Usuário ou senha incorretos`);
-//         }
-//       });
-//     })
-//     .catch((error) => console.log(`Erro ao executar API -> ${error}`));
-// };
